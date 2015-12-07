ced467666094a6463bfab181d08e5e146c6b794c
sircamus <ing.sergio.camus@gmail.com>
2015-12-07 00:58:37 -0600

    Device Ready Final

--- js/controllers.js
+++ js/controllers.js
@@ -1,86 +1,41 @@
-angular.module('FindMe.controllers', [])
+angular.module('FindMe.controllers', ['firebase'])
 
-<<<<<<< HEAD
-=======
-// APP - RIGHT MENU
-.controller('AppCtrl', function($scope){
-
-})
-
->>>>>>> parent of fb4c10d... Login working!!!
 
 //Controllers
 
-.controller('HomeCtrl', function($scope, $ionicActionSheet, $state, $ionicSideMenuDelegate, $ionicHistory){
+.controller('HomeCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray, $ionicActionSheet, $ionicSideMenuDelegate, $state, $ionicHistory){
 
-<<<<<<< HEAD
 
-<<<<<<< HEAD
 	//Model
 	$scope.model = { name: 'model' };	
 
-=======
-//Model
-	$scope.model = { name: 'model' };		
->>>>>>> parent of 81bd032... Login changes
+
 
 	//Define Firebase collection
-<<<<<<< HEAD
-<<<<<<< HEAD
 	var ref = new Firebase('https://fmdevice.firebaseio.com/users/finds');
-=======
-	var ref = new Firebase('https://findmedb.firebaseio.com/finds');
->>>>>>> parent of fb4c10d... Login working!!!
-=======
-	var ref = new Firebase('https://fmtrmp1.firebaseio.com/users/finds');
->>>>>>> parent of 9361fc1... Device ready
 	$scope.finds = $firebaseArray(ref);
-=======
->>>>>>> parent of 2d305b2... maps and insert working
+
 
 	//Back 
   	$scope.myGoBack = function() {
     $ionicHistory.goBack();
-<<<<<<< HEAD
  	
 	}
 
-<<<<<<< HEAD
 	  //Remove Rental
 	$scope.remove = function(){
 		
 		//GET DB Instance
-		var ref = new Firebase('https://fmtrmp1.firebaseio.com/users/finds/');
+		var ref = new Firebase('https://fmdevice.firebaseio.com/users/finds/');
 		ref.remove();
 	}
 }])
 
 
-<<<<<<< HEAD
-=======
-	//Menu Toggle
-//     $scope.toggleMenu = function(){
-//     $ionicSideMenuDelegate.toggleLeft();
-// }
-
-}])
->>>>>>> parent of fb4c10d... Login working!!!
 
 .controller('SideCtrl', function($scope, $ionicActionSheet,$ionicSideMenuDelegate, $state){
-=======
- 
-}
-
-	//Menu Toggle
-    $scope.toggleMenu = function(){
-    $ionicSideMenuDelegate.toggleLeft();
-}
-
->>>>>>> parent of 2d305b2... maps and insert working
 
 
-=======
->>>>>>> parent of 81bd032... Login changes
 	$scope.showLogOutMenu = function() {
 		// Show the action sheet
 		var hideSheet = $ionicActionSheet.show({
@@ -104,7 +59,7 @@ angular.module('FindMe.controllers', [])
 			destructiveButtonClicked: function(){
 				//Called when the destructive button is clicked.
 				//Return true to close the action sheet, or false to keep it opened.
-				var ref = new Firebase("https://fmtrmp1.firebaseio.com/");
+				var ref = new Firebase("https://fmdevice.firebaseio.com/");
 				ref.unauth();
 				$state.go('facebook-sign-in');
 			}
@@ -113,150 +68,73 @@ angular.module('FindMe.controllers', [])
 	};
 })
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 
 .controller('AddCtrl', ['$scope', '$firebaseArray','$ionicPopup', '$state', function($scope, $firebaseArray, $ionicSideMenuDelegate, $state, $ionicPopup, $location, $authClientProvider){
-=======
-.controller('AddCtrl', ['$scope', '$firebaseArray','$ionicPopup', '$state', function($scope, $firebaseArray, $ionicSideMenuDelegate, $state, $ionicPopup, $location){
->>>>>>> parent of fb4c10d... Login working!!!
 
 	
 	//Model
-<<<<<<< HEAD
 	$scope.model = { name: 'model' };	
-=======
-.controller('AddCtrl', ['$scope', '$firebaseArray', function($scope, $firebaseArray, $ionicSideMenuDelegate){
-
->>>>>>> parent of 2d305b2... maps and insert working
 
 	//Define Firebase collection
-<<<<<<< HEAD
 	var ref = new Firebase("https://fmdevice.firebaseio.com/users/finds");
-=======
-	$scope.model = { name: 'model' };		
-
-	//Define Firebase collection
-	var ref = new Firebase('https://findmedb.firebaseio.com/finds');
->>>>>>> parent of fb4c10d... Login working!!!
-=======
-	var ref = new Firebase("https://fmtrmp1.firebaseio.com/users/finds");
->>>>>>> parent of 9361fc1... Device ready
 	$scope.finds = $firebaseArray(ref);
 
-<<<<<<< HEAD
 	$scope.loc = {};
 
-	// $scope.positions = [];
 
-	// $scope.$on('mapInitialized', function(event, map) {
-	// $scope.map = map;
-	// });
-
-	// $scope.centerOnMe= function(){
-	// $scope.positions = [];
-	   
-
- //    navigator.geolocation.getCurrentPosition(function(position) {
- //    var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
- //    $scope.positions.push({lat: pos.k,lng: pos.B});
- //    console.log(pos);
-
- //    $scope.map.setCenter(pos);
- //    });
-
- //  }
-
-
-
-=======
->>>>>>> parent of 2d305b2... maps and insert working
 	$scope.addFind = function(){
+
 		console.log('Adding find');
-<<<<<<< HEAD
 
 		if ($scope.model.nombre) { var nombre = $scope.model.nombre; } else { var nombre = null; }
 		if ($scope.model.notas) { var notas = $scope.model.notas; } else { var notas = ''; }
-<<<<<<< HEAD
 		if ($scope.model.choice) { var avatar = $scope.model.choice; } else { var avatar = 'default.png'; }
 		if ($scope.loc.latitude) { var lat = $scope.loc.latitude; } else { var lat = '19.390858961426655'; }
 		if ($scope.loc.longitude) { var lgt = $scope.loc.longitude; } else { var lgt = '-99.14361265000002'; }
-=======
-		if ($scope.nombre) { var nombre = $scope.nombre; } else { var nombre = 'NOMBRE NULL'; }
-		if ($scope.notas) { var notas = $scope.notas; } else { var notas = null; }
-		if ($scope.foto) { var foto = $scope.foto; } else { var foto = 'http://img2.wikia.nocookie.net/__cb20130511180903/legendmarielu/images/b/b4/No_image_available.jpg'; }
-		if ($scope.lugar) { var lugar = $scope.lugar; } else { var lugar = null; }
->>>>>>> parent of 2d305b2... maps and insert working
-=======
-		if ($scope.foto) { var foto = $scope.foto; } else { var foto = 'http://img2.wikia.nocookie.net/__cb20130511180903/legendmarielu/images/b/b4/No_image_available.jpg'; }
-
-		if ($scope.loc.latitude) { var lat = $scope.loc.latitude; } else { var lat = null; }
-		if ($scope.loc.longitude) { var lgt = $scope.loc.longitude; } else { var lgt = null; }
-		if ($scope.loc.desc) { var des = $scope.loc.desc; } else { var des = null; }
->>>>>>> parent of 81bd032... Login changes
 
 		$scope.finds.$add({
 			nombre : nombre,
 			notas : notas,
-<<<<<<< HEAD
 			avatar : avatar, 
 			lat : lat,
 			lgt : lgt,
-=======
-			foto : foto, 
-			lugar : lugar, 
->>>>>>> parent of 2d305b2... maps and insert working
 			fecha : Firebase.ServerValue.TIMESTAMP
+
 		}).then(function(ref){
 			var id = ref.key();
+			$scope.loc = {};
+			// $scope.showAlert();
 			console.log('Find added with name: '+nombre);
 			console.log('Find added with ID: '+id);
-		});
+		}, function(error) {
+			console.log("Error:", error);
+		})
 
+		alert('¡Se guardó tu nuevo Find!');
 		clearFields();
 
 		$state.go('app.home');
 		
-		
+	
 	}
 
 	function clearFields(){
-<<<<<<< HEAD
 		console.log('Clearing fields...');
 		$scope.model.nombre = '';
 		$scope.model.notas = '';
 		$scope.model.choice = null;
 	}
-
-<<<<<<< HEAD
-=======
-		console.log('Clearing all fields...');
-
-		$scope.nombre = '';
-		$scope.notas = '';
-	}
->>>>>>> parent of 2d305b2... maps and insert working
-=======
-	// $scope.showAlert = function() {
- //    $ionicPopup.alert({
- //        title: 'Find Me',
- //        template: '¡Se guardó tu nuevo Find!'
- //    });
-	// }
->>>>>>> parent of fb4c10d... Login working!!!
-
 }])
+
+
 
 .controller('WelcomeCtrl', function($scope, $ionicModal, $state){
 	$scope.bgs = ["http://lorempixel.com/640/1136", "https://dl.dropboxusercontent.com/u/30873364/envato/ionFB/ion-fb-feed.gif"];
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	// $scope.facebookSignIn = function(){
 	// 	console.log("doing facebbok sign in");
-	// 	var ref = new Firebase("https://fmtrmp1.firebaseio.com/");
+	// 	var ref = new Firebase("https://fmdevice.firebaseio.com/");
 	// 	ref.authWithOAuthPopup("facebook", function(error, authData) {
 	// 		if (error) {
 	// 			console.log("Login Failed!", error);
@@ -269,51 +147,18 @@ angular.module('FindMe.controllers', [])
 	// 			$state.go('app.home');
 	// 		}
 	// 	});
-=======
-	$scope.facebookSignIn = function(){
-		console.log("doing facebbok sign in");
-		var ref = new Firebase("https://findmedb.firebaseio.com/");
-		ref.authWithOAuthPopup("facebook", function(error, authData) {
-			if (error) {
-				console.log("Login Failed!", error);
-			} else {
-				console.log("Authenticated successfully with payload:", authData)
-				$state.go('app.home');
-
-				console.log("Logged in as:", authData.facebook.displayName);
-				console.log("Profile Pic URL:", authData.facebook.profileImageURL);
-
-				var FBconn = $scope.userName = authData.facebook.displayName;
-			}
-		});
->>>>>>> parent of fb4c10d... Login working!!!
 		
 	// };
 })
 
-.controller('CreateAccountCtrl', function($scope, $state){
-	$scope.doSignUp = function(){
-		console.log("doing sign up");
-		alert('¡Listo! Tu cuenta ha sido creada');
-		$state.go('app.home');
-	};
-})
 
-<<<<<<< HEAD
-=======
-	$scope.facebookSignIn = function(){
-		console.log("doing facebbok sign in");
-		$state.go('app.home');
-	};
->>>>>>> parent of 81bd032... Login changes
 
 .controller('CreateAccountCtrl', ['$scope', '$state', function($scope, $state, $authClientProvider){
 
 	//Define Firebase collection
-	var ref = new Firebase('https://fmtrmp1.firebaseio.com/users');
+	var ref = new Firebase('https://fmdevice.firebaseio.com/users');
 
 
-<<<<<<< HEAD
 	$scope.doSignUp = function(email, password){
 		console.log("Creando cuenta...");
 		var email = $("#register-email").val();
@@ -331,20 +176,6 @@ angular.module('FindMe.controllers', [])
 			}
 		})
 	}
-=======
-.controller('CreateAccountCtrl', function($scope, $state){
-	$scope.doSignUp = function(){
-		console.log("doing sign up");
-		$state.go('app.home');
-	};
-})
-
-.controller('WelcomeBackCtrl', function($scope, $ionicModal, $state){
-	$scope.doLogIn = function(){
-		console.log("doing log in");
-		$state.go('app.home');
-	};
->>>>>>> parent of 81bd032... Login changes
 
 	function getName(authData) {
   switch(authData.provider) {
@@ -355,7 +186,7 @@ angular.module('FindMe.controllers', [])
 
 	function doLogin(email, password) {
 	var isNewUser = true;
-	var ref = new Firebase('https://fmtrmp1.firebaseio.com/');
+	var ref = new Firebase('https://fmdevice.firebaseio.com/');
 	ref.onAuth(function(authData) {
 	  if (authData && isNewUser) {
 	    // save the user's profile into the database so we can list users,
@@ -393,50 +224,15 @@ angular.module('FindMe.controllers', [])
 	});
 	}
 }])
-=======
-.controller('WelcomeBackCtrl', function($scope, $ionicModal, $state){
-	$scope.doLogIn = function(){
-		console.log("doing log in");
-		$state.go('app.home');
-	};
 
-	$ionicModal.fromTemplateUrl('views/partials/forgot-password.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.forgot_password_modal = modal;
-  });
 
-  $scope.showForgotPassword = function() {
-    $scope.forgot_password_modal.show();
-  };
->>>>>>> parent of fb4c10d... Login working!!!
+.controller('WelcomeBackCtrl', ['$scope', '$state', function($scope, $state, $authClientProvider){
 
-	$scope.requestNewPassword = function() {
-    console.log("requesting new password");
-		$state.go('app.home');
-  };
-
-  // //Cleanup the modal when we're done with it!
-  // $scope.$on('$destroy', function() {
-  //   $scope.modal.remove();
-  // });
-  // // Execute action on hide modal
-  // $scope.$on('modal.hidden', function() {
-  //   // Execute action
-  // });
-  // // Execute action on remove modal
-  // $scope.$on('modal.removed', function() {
-  //   // Execute action
-  // });
-})
-
-<<<<<<< HEAD
 	//Model
 	$scope.model = { name: 'model' };	
 
 	$scope.doLogIn = function(email, password) {
-	var ref = new Firebase('https://fmtrmp1.firebaseio.com/users');
+	var ref = new Firebase('https://fmdevice.firebaseio.com/users');
 	var email = $("#login-email").val();
     var password = $("#login-password").val();
 
@@ -471,6 +267,3 @@ angular.module('FindMe.controllers', [])
 	});
 	}
 }]);
-=======
-;
->>>>>>> parent of fb4c10d... Login working!!!
